90a3ae9872e67fca253de5ab1e35d02a777f832d
cchayanthithan <kumarchayan31@gmail.com>
2025-04-22 19:32:13 +0530

    build for deploy on vercel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ServiceComponent } from './pages/service/service.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'service', component: ServiceComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'home', component: HomeComponent },
+    { path: '**', redirectTo: '/home' },
+];
